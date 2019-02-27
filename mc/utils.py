d0da36d1c5e97814814257309e27e88d15b6d115
LeoQuote <leo.quote@hotmail.com>
2019-02-27 20:03:07 +0800

    增加traceback ， 更改为args

--- mc/utils.py
+++ mc/utils.py
@@ -1,13 +1,15 @@
 import subprocess
 import logging
+import traceback
 
 def check_server(workdir):
     check_command = "./minecraft status"
     try:
-        status = subprocess.run(check_command, cwd=workdir, stdout=subprocess.PIPE)
+        status = subprocess.run(check_command, args=['status'], cwd=workdir, stdout=subprocess.PIPE)
     except FileNotFoundError:
         server_status = 'unknown'
         logging.error('File not found: {}'.format(workdir))
+        logging.error(traceback.format_exc())
         return server_status
     logging.info(status.stdout)
     # server_status = 'not ok'
